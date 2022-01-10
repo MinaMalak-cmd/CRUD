020699533ee8cb50214bd7765cbb72831c2384de
MinaMalak-cmd <engmina15@yahoo.com>
2022-01-10 16:35:04 +0200

    add routing and main UI layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import './App.css';
+import { Switch, Route, Link } from "react-router-dom";
+import AddUser from "./components/AddUser";
+import User from "./components/User";
+import UsersList from "./components/UsersList";
 
 function App() {
   return (
-    <div className="App">
-      hjjy
+    <div>
+    <nav className="navbar navbar-expand navbar-dark bg-dark">
+      <a href="/Users" className="navbar-brand">
+        Mina
+      </a>
+      <div className="navbar-nav mr-auto">
+        <li className="nav-item">
+          <Link to={"/Users"} className="nav-link">
+            Users
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to={"/add"} className="nav-link">
+            Add
+          </Link>
+        </li>
+      </div>
+    </nav>
+
+    <div className="container mt-3">
+      <Switch>
+        <Route exact path={["/", "/Users"]} component={UsersList} />
+        <Route exact path="/add" component={AddUser} />
+        <Route path="/Users/:id" component={User} />
+      </Switch>
     </div>
+  </div>
   );
 }
 
